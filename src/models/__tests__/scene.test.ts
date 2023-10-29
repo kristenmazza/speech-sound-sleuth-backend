1752bdc25772da6719914a77f28462b76c113d26
kristenmazza <kbmazza@gmail.com>
2023-10-28 19:16:26 -0700

    Simplify code

--- src/models/__tests__/scene.test.ts
+++ src/models/__tests__/scene.test.ts
@@ -23,33 +23,15 @@ describe('Scene model', () => {
       imageUrl:
         'https://kristen-mazza-blog-images.s3.us-west-1.amazonaws.com/uploads/busystreet.png',
       hiddenImages: [
-        '65307f43ede6531308401a6d',
-        '65307f43ede6531308401a6e',
-        '65307f43ede6531308401a70',
-        '65307f43ede6531308401a6f',
+        new mongoose.Types.ObjectId('65307f43ede6531308401a6d'),
+        new mongoose.Types.ObjectId('65307f43ede6531308401a6e'),
+        new mongoose.Types.ObjectId('65307f43ede6531308401a70'),
+        new mongoose.Types.ObjectId('65307f43ede6531308401a6f'),
       ],
     };
 
     // Act
     const validScene = new Scene(sceneData);
-    const hiddenImageId0 = new mongoose.Types.ObjectId(
-      sceneData.hiddenImages[0]
-    );
-    const hiddenImageId1 = new mongoose.Types.ObjectId(
-      sceneData.hiddenImages[1]
-    );
-    const hiddenImageId2 = new mongoose.Types.ObjectId(
-      sceneData.hiddenImages[2]
-    );
-    const hiddenImageId3 = new mongoose.Types.ObjectId(
-      sceneData.hiddenImages[3]
-    );
-
-    validScene.hiddenImages[0] = hiddenImageId0;
-    validScene.hiddenImages[1] = hiddenImageId1;
-    validScene.hiddenImages[2] = hiddenImageId2;
-    validScene.hiddenImages[3] = hiddenImageId3;
-
     const savedScene = await validScene.save();
 
     // Assert
@@ -58,7 +40,7 @@ describe('Scene model', () => {
     expect(savedScene.sound).toBe(sceneData.sound);
     expect(savedScene.imageUrl).toBe(sceneData.imageUrl);
     expect(savedScene.hiddenImages[0].toString()).toBe(
-      hiddenImageId0.toString()
+      '65307f43ede6531308401a6d'
     );
   });
 });
